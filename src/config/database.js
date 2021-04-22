f013b6a0ef43c25438a59d34d519830330817442
weversonneri <weversonneri@outlook.com>
2021-04-21 22:17:15 -0300

    prod db config

--- src/config/database.js
+++ src/config/database.js
@@ -1,12 +1,24 @@
+require('dotenv').config();
+
 module.exports = {
-  dialect: 'postgres',
-  host: 'localhost',
-  username: 'postgres',
-  password: 'docker',
-  database: 'psinmain',
-  define: {
-    timestamps: true,
-    underscored: true,
-    underscoredAll: true,
+  development: {
+    dialect: 'postgres',
+    host: 'localhost',
+    username: 'postgres',
+    password: 'docker',
+    database: 'psinmain',
+    define: {
+      timestamps: true,
+      underscored: true,
+      underscoredAll: true,
+    },
+  },
+  test: {
+    url: process.env.TEST_DATABASE_URL,
+    dialect: 'postgres',
+  },
+  production: {
+    url: process.env.DATABASE_URL,
+    dialect: 'postgres',
   },
 };
