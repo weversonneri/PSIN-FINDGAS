f9704cad116c5f4255561aed7020b4af80db46a9
weversonneri <weversonneri@outlook.com>
2021-05-22 00:52:11 -0300

    postgres ssl config2

--- src/config/database.js
+++ src/config/database.js
@@ -2,16 +2,8 @@ require('dotenv').config();
 
 module.exports = {
   development: {
+    url: process.env.DATABASE_URL,
     dialect: 'postgres',
-    host: 'localhost',
-    username: 'postgres',
-    password: 'docker',
-    database: 'psinmain',
-    define: {
-      timestamps: true,
-      underscored: true,
-      underscoredAll: true,
-    },
   },
   test: {
     url: process.env.TEST_DATABASE_URL,
@@ -20,11 +12,8 @@ module.exports = {
   production: {
     url: process.env.DATABASE_URL,
     dialect: 'postgres',
-    dialectOptions: {
-      ssl: {
-        require: true, // This will help you. But you will see nwe error
-        rejectUnauthorized: false, // This line will fix new error
-      },
+    ssl: {
+      rejectUnauthorized: false,
     },
   },
 };
