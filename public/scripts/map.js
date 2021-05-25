37278fa86f05d253ac72dc88b6ba6010bf1569dd
weversonneri <weversonneri@outlook.com>
2021-05-25 13:20:22 -0300

    map popup

--- public/scripts/map.js
+++ public/scripts/map.js
@@ -33,6 +33,12 @@ const popupOptions = {
   closeButton: false,
 };
 
+const popupOptionsPro = {
+  maxWidth: '500',
+  className: 'custom-pro',
+  closeButton: false,
+};
+
 const teste = document.getElementById('test');
 
 const getData = async () => {
@@ -48,24 +54,58 @@ const getData = async () => {
         const marker = L.marker([item.latitude, item.longitude], { icon: mapIconPro }).addTo(mymap);
 
         const popup = L.popup()
-          .setContent(teste);
+          .setContent(`<div id="" class="flex max-w-xs -m-1.5">
+          <div class="">
+            <div class=" space-y-0.5">
+              <p class=" text-xs text-gray-900">Gas de cozinha 13Kg</p>
+              <p class=" text-base md:text-lg text-gray-900 font-bold">R$ 85,00</p>
+              <p class=" text-sm md:text-base text-gray-900 break-words">${item.name}</p>
+            </div>
+
+            <a href="https://api.whatsapp.com/send?phone=${item.phone}&text=${item.name}" target="_blank"
+              class=" flex items-center space-x-1">
+              <i class="fab fa-whatsapp stroke-current text-green-800 text-sm sm:text-base"></i>
+              <p class=" text-xs sm:text-sm">${item.phone}</p>
+            </a>
+          </div>
+          <a href="/provider-detail/${item.id}"
+            class=" ml-4 my-4 p-1 flex items-center bg-blue-500 hover:bg-blue-600 rounded-md " title="Detalhes">
+            <svg class="h-5 w-5 stroke-current text-white" xmlns="http://www.w3.org/2000/svg" fill="none"
+              viewBox="0 0 24 24" stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7" />
+            </svg>
+          </a>
+        </div>`);
         marker.bindTooltip('Anúncio').openTooltip();
 
-        marker.bindPopup(popup, popupOptions);
+        marker.bindPopup(popup, popupOptionsPro).openPopup();
       }
 
       if (item.User.subscription === 'N') {
         const marker = L.marker([item.latitude, item.longitude], { icon: mapIcon }).addTo(mymap);
 
         const popup = L.popup()
-          .setContent(`<div class="flex items-center justify-between -mx-1.5">
-              <p class=" text-xs md:text-sm text-red-900">${item.User.subscription}</p>
-              <a href="/provider-detail/${item.id}" class=" ml-2 p-1 bg-blue-500 hover:bg-blue-600 rounded-md " title="Detalhes">
-                <svg class="h-5 w-5 stroke-current text-white" xmlns="http://www.w3.org/2000/svg"  fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7" />
-                </svg>
-              </a>
-              </div>
+          .setContent(`<div class="flex max-w-xs -m-1.5">
+          <div class=" -space-y-3 ">
+            <div class=" -space-y-0.5 ">
+              <p class=" text-xs text-gray-800">Gas de cozinha 13Kg</p>
+              <p class=" text-sm text-gray-800 font-bold">R$ 85,00</p>
+              <p class=" text-xs text-gray-900 break-words">Deposito nome</p>
+            </div>
+
+            <a class=" flex items-center space-x-1">
+              <i class="fas fa-phone-alt stroke-current text-gray-800 text-xs"></i>
+              <p class=" text-xs text-gray-800">(61)9 999-9999</p>
+            </a>
+          </div>
+          <a href="/provider-detail/${item.id}"
+            class=" ml-4 my-4 p-1 flex items-center bg-blue-500 hover:bg-blue-600 rounded-md " title="Detalhes">
+            <svg class="h-4 w-4 stroke-current text-white" xmlns="http://www.w3.org/2000/svg" fill="none"
+              viewBox="0 0 24 24" stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7" />
+            </svg>
+          </a>
+        </div>
         `);
         // marker.bindTooltip(item.subcription).openTooltip();
 
