843cea4409fc6e6666af2ad158274020aa698ccc
weversonneri <weversonneri@outlook.com>
2021-06-01 18:18:44 -0300

    proxy

--- src/server.js
+++ src/server.js
@@ -23,6 +23,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(flash());
+app.enable('trust proxy');
 app.use(session({
   // eslint-disable-next-line global-require
   store: new (require('connect-pg-simple')(session))(),
